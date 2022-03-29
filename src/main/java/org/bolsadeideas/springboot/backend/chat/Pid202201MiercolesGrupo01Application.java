14e18b81a26861f9b7169f6542274423753cd591
arangurenalonso <69709834+arangurenalonso@users.noreply.github.com>
2022-03-28 19:40:44 -0500

    First Commit

---
+++ src/main/java/org/bolsadeideas/springboot/backend/chat/Pid202201MiercolesGrupo01Application.java
@@ -0,0 +1,13 @@
+package org.bolsadeideas.springboot.backend.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pid202201MiercolesGrupo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pid202201MiercolesGrupo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/bolsadeideas/springboot/backend/chat/Pid202201MiercolesGrupo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bolsadeideas.springboot.backend.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pid202201MiercolesGrupo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
