b427c9aa96735b71a30d9860f509f5bd9586723b
ginoalimo <ginoalgarcia@gmail.com>
2022-03-30 16:45:02 -0500

    Prueba numero 1 crear clase Persona.

---
+++ src/main/java/org/bolsadeideas/springboot/backend/chat/entity/Persona.java
@@ -0,0 +1,11 @@
+package org.bolsadeideas.springboot.backend.chat.entity;
+
+public class Persona {
+	
+	public String nombre;
+	public String apellido;
+	public String telefono;
+	public String dni;
+	public String email;
+
+}
